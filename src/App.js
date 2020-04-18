94a8b20f8bfebae2470da056d688afc6ad7e95ff
Ricardo <ricardo@brittes.com.br>
2020-04-17 23:46:52 -0300

    Correção do Bug

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MagicContainer from "./components/MagicContainer";
 import cardsReducer from "./reducer";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import Page1 from "./components/Card";
+import Page1 from "./components/Page1";
 import Page2 from "./components/Page2";
 import Page3 from "./components/Page3";
 import Page4 from "./components/Page4";
